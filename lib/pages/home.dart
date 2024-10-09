3c4fc77d50df28223e2693c834346da189304880
Sergio M. Mahakala <sergio@labsma.com>
2024-10-09 10:24:28 -0600

    scaffold for dashboard

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
+import 'package:easy_dashboard/easy_dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:optifreight/utils/utils.dart';
-import 'package:optifreight/pages/pages.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -21,46 +22,128 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
-        child: ListView(
-          children: <Widget>[
-            const SizedBox(
-              height: 50,
+        child: EasyDashboard(
+          controller: EasyAppController(),
+          navigationIcon: const Icon(Icons.menu, color: Colors.white),
+          appBarActions: [
+            IconButton(
+              icon: const Icon(
+                FontAwesomeIcons.house,
+                color: Colors.white,
+              ),
+              onPressed: () {},
             ),
-            _buildAppBar(),
-            const OnboardingPage(),
+            IconButton(
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
           ],
-        ),
-      ),
-    );
-  }
-
-  SizedBox _buildAppBar() {
-    return SizedBox(
-      height: 250,
-      child: Row(
-        children: [
-          Container(
-            width: 250,
-            padding: const EdgeInsets.only(left: 50),
-            child: const Column(
-              children: [
-                Image(
-                  image: AssetImage(
-                    'images/logo.png',
-                  ),
+          centerTitle: true,
+          appBarColor: AppColors.tertiary,
+          sideBarColor: Colors.grey.shade100,
+          tabletView: const TabletView(
+            fullAppBar: false,
+            border: BorderSide(width: 0.5, color: Colors.grey),
+          ),
+          desktopView: const DesktopView(
+            fullAppBar: true,
+            border: BorderSide(width: 0.5, color: Colors.grey),
+          ),
+          drawer: (Size size, Widget? child) {
+            return EasyDrawer(
+              iconColor: Colors.black.withBlue(20),
+              hoverColor: Colors.grey.shade300,
+              tileColor: Colors.grey.shade100,
+              selectedColor: Colors.white,
+              selectedIconColor: Colors.white,
+              textColor: Colors.black.withBlue(20),
+              selectedTileColor: AppColors.primary,
+              tiles: [
+                SideBarTile(
+                  name: "Home",
+                  body: const Text("Home"),
+                  icon: Icons.home,
+                  title: const Text("Home"),
                 ),
-                Text(
-                  'FEDERATED FREIGHT NETWORK',
-                  style: TextStyle(
-                    fontSize: 13,
-                    fontWeight: FontWeight.w900,
-                  ),
+                SideBarTile(
+                  name: "Purchase Orders",
+                  body: const Text("Orders"),
+                  icon: FontAwesomeIcons.box,
+                  title: const Text("Purchase Orders"),
+                ),
+                SideBarTile(
+                  name: "Jobs",
+                  body: const Text("Jobs"),
+                  icon: FontAwesomeIcons.truckFast,
+                  title: const Text("Jobs"),
+                ),
+                SideBarTile(
+                  name: "Routes",
+                  body: const Text("Routes"),
+                  icon: FontAwesomeIcons.route,
+                  title: const Text("Routes"),
+                ),
+                SideBarTile(
+                  name: "OPTI Tokens",
+                  body: const Text("Orders"),
+                  icon: FontAwesomeIcons.coins,
+                  title: const Text("OPTI Tokens"),
                 ),
               ],
-            ),
-          ),
-          const Spacer(),
-        ],
+              topWidget: SideBox(
+                scrollable: true,
+                height: 200,
+                child: Container(
+                  width: 200,
+                  padding: const EdgeInsets.only(top: 25),
+                  child: const Column(
+                    children: [
+                      Image(
+                        image: AssetImage(
+                          'images/logo.png',
+                        ),
+                      ),
+                      Text(
+                        'FEDERATED FREIGHT NETWORK',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              bottomWidget: SideBox(
+                scrollable: false,
+                height: 88,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    label: const Text("Connect Wallet"),
+                    icon: const Icon(FontAwesomeIcons.wallet),
+                  ),
+                ),
+              ),
+              bottomSmallWidget: const SideBox(
+                height: 50,
+                child: Text("Bottom Small Widget"),
+              ),
+              topSmallWidget: SideBox(
+                height: 50,
+                child: Image.asset("assets/images/icon.png"),
+              ),
+              size: size,
+              onTileTapped: (body) {
+                debugPrint("Tapped: ${body.title}");
+              },
+            );
+          },
+        ),
       ),
     );
   }
---
+++ lib/pages/landing.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:optifreight/pages/pages.dart';
+import 'package:optifreight/utils/utils.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: const BoxDecoration(
+          // Set a gradient background with purple and blue
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              AppColors.tertiary,
+              AppColors.secondary,
+            ],
+          ),
+        ),
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(
+              height: 50,
+            ),
+            _buildAppBar(),
+            const OnboardingPage(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox _buildAppBar() {
+    return SizedBox(
+      height: 250,
+      child: Row(
+        children: [
+          Container(
+            width: 250,
+            padding: const EdgeInsets.only(left: 50),
+            child: const Column(
+              children: [
+                Image(
+                  image: AssetImage(
+                    'images/logo.png',
+                  ),
+                ),
+                Text(
+                  'FEDERATED FREIGHT NETWORK',
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
